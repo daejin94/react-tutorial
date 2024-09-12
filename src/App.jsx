97e14e7ca7bd66c31816e8e321c11af7b21b851f
daejin94 <eowls1358@gmail.com>
2024-09-12 19:47:08 +0900

    setState는 비동기다

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,30 @@ import { useState } from 'react'
 import '@/App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [level, setLevel] = useState(1)
+  const [title, setTitle] = useState('Novice')
+
+  const levelup_not_working = () => {
+    console.log('이전 상태: ', level)
+    setLevel((previous) => {
+      return previous + 1
+    })
+
+    setLevel(level + 1)
+
+    if (level >= 30) {
+      setTitle('2차 전직')
+    } else if (level >= 15) {
+      setTitle('1차 전직')
+    }
+  }
 
   return (
     <>
-      <div>{count}</div>
+      <div>{level}</div>
+      <div>{title}</div>
       <div>
-        <button
-          onClick={(e) => {
-            setCount(count + 1)
-          }}
-        ></button>
+        <button onClick={levelup_not_working}></button>
       </div>
     </>
   )
