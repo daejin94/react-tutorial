5ee8f0bd6d2c4a9af71b58ec0c50955d195a0432
daejin94 <eowls1358@gmail.com>
2024-09-18 17:30:51 +0900

    6-2 HTML 요소 중 Unordered List 의 React Component 화

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import "./App.css";
 
+function UnorderedList() {
+  return (
+    <div>
+      <ul>
+        <li>Unordered List Item 1</li>
+        <li>Unordered List Item 2</li>
+        <li>Unordered List Item 3</li>
+      </ul>
+    </div>
+  );
+}
+
 function App() {
   return (
     <>
       <div>
-        <ul>
-          <li>Unordered List Item 1</li>
-          <li>Unordered List Item 2</li>
-          <li>Unordered List Item 3</li>
-        </ul>
+        <UnorderedList />
         <ol>
           <li>Ordered List Item 1</li>
           <li>Ordered List Item 2</li>
